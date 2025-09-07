6f889647fcdd3ec071964553f0a4ea966f3be5f2
Carlos-Rodriguez98 <cs.rodrigueza1@uniandes.edu.co>
2025-09-07 17:15:50 -0500

    Ajuste env.go para solo intentar cargar el archivo .env si las variables están vacías

--- services/auth-service/config/env.go
+++ services/auth-service/config/env.go
@@ -21,12 +21,20 @@ type EnvConfig struct {
 var AppConfig EnvConfig
 
 func LoadEnv() {
-	prueba := os.Getenv("DB_HOST")
-	log.Printf("Validación DB_HOST: %v", prueba)
-	//Carga del archivo .env si existe
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("No se pudo cargar .env, usando variables de entorno")
+	log.Printf("DEBUG ENV -> DB_HOST=%s DB_USER=%s DB_NAME=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_NAME"))
+
+	// Solo intenta cargar .env si DB_HOST no existe en el sistema
+	if os.Getenv("DB_HOST") == "" {
+		if err := godotenv.Load(); err == nil {
+			log.Println("Variables cargadas desde archivo .env")
+		} else {
+			log.Println("No se encontró archivo .env, usando solo variables del sistema")
+		}
+	} else {
+		log.Println("Detectadas variables del sistema, se omite carga de archivo .env")
 	}
 
 	//Valida variable de entorno de puerto de BD
