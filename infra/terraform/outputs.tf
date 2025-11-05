43b724359e6ae1f2de7a505c270873c1aa01d418
Carlos-Rodriguez98 <cs.rodrigueza1@uniandes.edu.co>
2025-11-04 20:48:54 -0500

    Ajuste de salidas para relacionar información de la nueva subred pública y el alb

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,16 +1,18 @@
 output "vpc_id" { value = aws_vpc.main.id }
 output "public_subnet_id" { value = aws_subnet.public.id }
+output "public_subnet_b_id" { value = aws_subnet.public_b.id }
 output "private_subnet_a" { value = aws_subnet.private_a.id }
 output "private_subnet_b" { value = aws_subnet.private_b.id }
 output "public_route_table_id" { value = aws_route_table.public.id }
 output "private_route_table_id" { value = aws_route_table.private.id }
 
 output "web_sg_id" { value = aws_security_group.web.id }
+output "alb_sg_id" { value = aws_security_group.alb.id }
 output "worker_sg_id" { value = aws_security_group.worker.id }
 output "rds_sg_id" { value = aws_security_group.rds.id }
 
-output "web_public_ip" { value = aws_instance.web.public_ip }
-output "web_instance_id" { value = aws_instance.web.id }
+#output "web_public_ip" { value = aws_instance.web.public_ip }
+#output "web_instance_id" { value = aws_instance.web.id }
 output "worker_private_ip" { value = aws_instance.worker.private_ip }
 output "worker_instance_id" { value = aws_instance.worker.id }
 output "s3_bucket_name" { value = aws_s3_bucket.storage.bucket }
@@ -22,7 +24,16 @@ output "rds_db_name" { value = aws_db_instance.main.db_name }
 
 output "web_instance_profile" { value = data.aws_iam_instance_profile.lab_instance_profile.name }
 
+# Application Load Balancer
+output "alb_dns_name" {
+  description = "DNS name del Application Load Balancer"
+  value       = aws_lb.main.dns_name
+}
+output "asg_name" {
+  description = "Nombre del Auto Scaling Group"
+  value       = aws_autoscaling_group.web.name
+}
 output "application_url" {
-  description = "URL de acceso a la aplicación web"
-  value       = "http://${aws_instance.web.public_ip}:8084"
+  description = "URL de acceso a la aplicación web vía ALB"
+  value       = "http://${aws_lb.main.dns_name}"
 }
