4457592de01bdd370b95fd6d485047d29218586a
Carlos-Rodriguez98 <cs.rodrigueza1@uniandes.edu.co>
2025-11-08 13:46:18 -0500

    Ajuste de esquema de la tabla videos

--- services/video-service/models/video.go
+++ services/video-service/models/video.go
@@ -12,25 +12,6 @@ const (
 	StatusDeleted   VideoStatus = "deleted"
 )
 
-/*
-type Video struct {
-	ID            string      `gorm:"column:video_id;primaryKey;size:36" json:"video_id"` // usa uuid
-	UserID        uint        `gorm:"column:user_id;index;not null" json:"-"`
-	Title         string      `gorm:"column:title;size:150;not null" json:"title"`
-	OriginalPath  string      `gorm:"column:original_path;size:255;not null" json:"-"`
-	ProcessedPath *string     `gorm:"column:processed_path;size:255" json:"-"`
-	Status        VideoStatus `gorm:"column:status;size:20;not null;default:'uploaded'" json:"status"`
-	UploadedAt    time.Time   `gorm:"column:uploaded_at;autoCreateTime" json:"uploaded_at"`
-	ProcessedAt   *time.Time  `gorm:"column:processed_at" json:"processed_at"`
-	Published     bool        `gorm:"column:published;default:false" json:"-"` // para gobernar borrado
-	PublishedAt   *time.Time  `gorm:"column:published_at" json:"-"`
-}
-
-func (Video) TableName() string {
-	return "app.videos"
-}
-*/
-
 type Video struct {
 	ID            uint        `gorm:"column:video_id;primaryKey;autoIncrement"`
 	UserID        uint        `gorm:"column:user_id;not null"`
@@ -45,5 +26,5 @@ type Video struct {
 }
 
 func (Video) TableName() string {
-	return "videos"
+	return "app.videos"
 }
