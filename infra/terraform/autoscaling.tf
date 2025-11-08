54c30c5caa1038d3769edc12538a5716f7d3d80a
Carlos-Rodriguez98 <cs.rodrigueza1@uniandes.edu.co>
2025-11-08 18:40:55 -0500

    Definición de capacidad minima y deseada de 2 instancias

--- infra/terraform/autoscaling.tf
+++ infra/terraform/autoscaling.tf
@@ -61,9 +61,9 @@ resource "aws_autoscaling_group" "web" {
   ]
   health_check_type         = "ELB"
   health_check_grace_period = 900
-  min_size                  = 1
+  min_size                  = 2
   max_size                  = 3
-  desired_capacity          = 1
+  desired_capacity          = 2
 
   launch_template {
     id      = aws_launch_template.web.id
