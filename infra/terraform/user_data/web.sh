cc0cdf8b8db1758290a294a33ea93bdd76ad44af
Carlos-Rodriguez98 <cs.rodrigueza1@uniandes.edu.co>
2025-11-08 22:00:30 -0500

    Ajuste de rama a clonar de GitHub

--- infra/terraform/user_data/web.sh
+++ infra/terraform/user_data/web.sh
@@ -69,7 +69,7 @@ cd /opt/anbapp
 
 # Clone repository
 echo "Clonando repositorio..."
-git clone -b feature/carlos-entrega3 https://github.com/Carlos-Rodriguez98/ANB-WebApp.git repo || {
+git clone -b dev https://github.com/Carlos-Rodriguez98/ANB-WebApp.git repo || {
     echo "Error al clonar repositorio. Verifica la URL y permisos."
     exit 1
 }
