21060758da2dfeb7f591bb65440e738d6e284199
David P <me@192.168.2.12>
2025-11-27 20:13:10 -0500

    Corrección de errores en el rol de IAM

--- infra/terraform/lambda.tf
+++ infra/terraform/lambda.tf
@@ -12,7 +12,8 @@ data "archive_file" "lambda_zip" {
 resource "aws_lambda_function" "worker_lambda" {
   filename         = data.archive_file.lambda_zip.output_path
   function_name    = "${var.project_name}-worker-lambda"
-  role             = aws_iam_role.lambda_exec_role.arn
+  role             = "arn:aws:iam::637423308545:role/LabRole" # data.aws_iam_instance_profile.lab_instance_profile.name
+ 
   handler          = "bootstrap"
   runtime          = "provided.al2023"
   architectures    = ["arm64"]
@@ -33,7 +34,7 @@ resource "aws_lambda_function" "worker_lambda" {
       DB_SSLMODE     = "require"
       JWT_SECRET     = var.jwt_secret
       S3_BUCKET_NAME = aws_s3_bucket.storage.id
-      AWS_REGION     = var.aws_region
+      # AWS_REGION     = var.aws_region
       SQS_QUEUE_URL  = aws_sqs_queue.video_processing.url
       SSM_BASE_PATH  = var.ssm_path
     }
@@ -41,9 +42,9 @@ resource "aws_lambda_function" "worker_lambda" {
 
   depends_on = [
     data.archive_file.lambda_zip,
-    aws_iam_role_policy_attachment.lambda_worker_custom_attachment,
-    aws_iam_role_policy_attachment.lambda_basic_execution_attachment,
-    aws_iam_role_policy_attachment.lambda_vpc_attachment
+    # aws_iam_role_policy_attachment.lambda_worker_custom_attachment,
+    # aws_iam_role_policy_attachment.lambda_basic_execution_attachment,
+    # aws_iam_role_policy_attachment.lambda_vpc_attachment
   ]
 }
 
@@ -53,62 +54,62 @@ resource "aws_lambda_event_source_mapping" "sqs_mapping" {
   batch_size       = 5
 }
 
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "${var.project_name}-lambda-exec-role"
+# resource "aws_iam_role" "lambda_exec_role" {
+#   name = "${var.project_name}-lambda-exec-role"
 
-  assume_role_policy = jsonencode({
-    Version   = "2012-10-17",
-    Statement = [{
-      Action    = "sts:AssumeRole",
-      Effect    = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
+#   assume_role_policy = jsonencode({
+#     Version   = "2012-10-17",
+#     Statement = [{
+#       Action    = "sts:AssumeRole",
+#       Effect    = "Allow",
+#       Principal = {
+#         Service = "lambda.amazonaws.com"
+#       }
+#     }]
+#   })
 
-  tags = {
-    Name = "${var.project_name}-lambda-exec-role"
-  }
-}
+#   tags = {
+#     Name = "${var.project_name}-lambda-exec-role"
+#   }
+# }
 
-resource "aws_iam_policy" "lambda_worker_policy" {
-  name        = "${var.project_name}-lambda-worker-policy"
-  description = "IAM policy for Lambda worker to access S3, SQS, and SSM"
+# resource "aws_iam_policy" "lambda_worker_policy" {
+#   name        = "${var.project_name}-lambda-worker-policy"
+#   description = "IAM policy for Lambda worker to access S3, SQS, and SSM"
 
-  policy = jsonencode({
-    Version   = "2012-10-17",
-    Statement = [
-      {
-        Action   = ["s3:GetObject", "s3:PutObject"],
-        Effect   = "Allow",
-        Resource = "${aws_s3_bucket.storage.arn}/*"
-      },
-      {
-        Action   = ["sqs:ReceiveMessage", "sqs:DeleteMessage", "sqs:GetQueueAttributes"],
-        Effect   = "Allow",
-        Resource = aws_sqs_queue.video_processing.arn
-      },
-      {
-        Action   = ["ssm:GetParametersByPath"],
-        Effect   = "Allow",
-        Resource = "arn:aws:ssm:${var.aws_region}:${data.aws_caller_identity.current.account_id}:parameter/${var.ssm_path}"
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version   = "2012-10-17",
+#     Statement = [
+#       {
+#         Action   = ["s3:GetObject", "s3:PutObject"],
+#         Effect   = "Allow",
+#         Resource = "${aws_s3_bucket.storage.arn}/*"
+#       },
+#       {
+#         Action   = ["sqs:ReceiveMessage", "sqs:DeleteMessage", "sqs:GetQueueAttributes"],
+#         Effect   = "Allow",
+#         Resource = aws_sqs_queue.video_processing.arn
+#       },
+#       {
+#         Action   = ["ssm:GetParametersByPath"],
+#         Effect   = "Allow",
+#         Resource = "arn:aws:ssm:${var.aws_region}:${data.aws_caller_identity.current.account_id}:parameter/${var.ssm_path}"
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "lambda_worker_custom_attachment" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = aws_iam_policy.lambda_worker_policy.arn
-}
+# resource "aws_iam_role_policy_attachment" "lambda_worker_custom_attachment" {
+#   role       = aws_iam_role.lambda_exec_role.name
+#   policy_arn = aws_iam_policy.lambda_worker_policy.arn
+# }
 
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution_attachment" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
+# resource "aws_iam_role_policy_attachment" "lambda_basic_execution_attachment" {
+#   role       = aws_iam_role.lambda_exec_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+# }
 
-resource "aws_iam_role_policy_attachment" "lambda_vpc_attachment" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
-}
+# resource "aws_iam_role_policy_attachment" "lambda_vpc_attachment" {
+#   role       = aws_iam_role.lambda_exec_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+# }
